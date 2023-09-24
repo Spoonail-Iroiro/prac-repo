1097768e2a50bba5e01fa834dc0e90a0d3e141d3
Spoonail-Iroiro <example@example.com>
2023-09-25 00:53:50 +0900

    Add import/export from/to saveStr to SaveManager

--- js/mod/mods/save-manager/modules/save-manager.js
+++ js/mod/mods/save-manager/modules/save-manager.js
@@ -1,6 +1,62 @@
 import FileImporter from './file-importer.js';
 import FileExporter from './file-exporter.js';
 
+async function zipData(data) {
+  return new Promise((resolve) => {
+    tWgm.tGameSave.zipDataWorker(data, resolve);
+  });
+}
+
+async function unzipData(zippedData) {
+  return new Promise((resolve) => {
+    tWgm.tGameSave.unzipDataWorker(zippedData, resolve);
+  });
+}
+
+async function convertUint8ArrayToBase64(data) {
+  return new Promise((resolve, reject) => {
+    const blob = new Blob([data], { type: 'application/gzip' });
+    const fr = new FileReader();
+    fr.addEventListener('load', (e) => {
+      resolve(e.target.result.substr(29));
+    });
+    fr.addEventListener('error', reject);
+    fr.readAsDataURL(blob);
+  });
+}
+
+async function toSaveMainFormat(data) {
+  const zipped = await zipData(data);
+  const b64encoded = await convertUint8ArrayToBase64(zipped);
+  return b64encoded;
+}
+
+async function fromSaveMainFormat(saveStr) {
+  const b64decoded = tWgm.tGameSave.convertBase64ToUint8Array(saveStr);
+  const unzipped = await unzipData(b64decoded);
+
+  return unzipped;
+}
+
+async function editSaveStr(saveStr, editFn) {
+  const { header, main, suspended, suspendedBackupHeader } =
+    tWgm.tGameSave.splitSaveData(saveStr);
+  const headerData = JSON.parse(header);
+  const mainData = await fromSaveMainFormat(main);
+  const [newHeaderData, newMainData] = editFn(headerData, mainData);
+  const newMain = await toSaveMainFormat(newMainData);
+  const newHeader = JSON.stringify(newHeaderData);
+
+  const newSaveStr = tWgm.tGameSave.convertSaveData({
+    header: newHeader,
+    main: newMain,
+    suspended,
+    suspendedBackupHeader,
+  });
+
+  return newSaveStr;
+}
+
 class SaveManager {
   constructor() {
     this.fileExporter = new FileExporter();
@@ -8,5 +64,31 @@ class SaveManager {
 
     // セーブデータ一時領域
     this.saveData = null;
+  }
+
+  async _import(saveStr) {
+    const { header, main, suspended, suspendedBackupHeader } =
+      tWgm.tGameSave.splitSaveData(saveStr);
+    this.saveData = {
+      header: JSON.parse(header),
+      main: await fromSaveMainFormat(main),
+      suspendedBackupHeader: JSON.parse(suspendedBackupHeader),
+      suspended: await fromSaveMainFormat(suspendedBackupHeader),
+    };
+  }
+
+  async _export() {
+    if (this.saveData === null)
+      throw new Error('セーブデータが一時領域に読み込まれていません');
+    const saveStr = tWgm.tGameSave.convertSaveData({
+      header: JSON.stringify(this.saveData.header),
+      main: await toSaveMainFormat(this.saveData.main),
+      suspendedBackupHeader: JSON.stringify(
+        this.saveData.suspendedBackupHeader
+      ),
+      suspended: await toSaveMainFormat(this.saveData.suspended),
+    });
+
+    return saveStr;
   }
 }
